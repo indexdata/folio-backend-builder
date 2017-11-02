b39230a9d294d10c44c3a4e3ec6c982bc08a5f44
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-11-02 10:10:19 +0100

    Add more inventory imports.

--- data/inventory/import.sh
+++ data/inventory/import.sh
@@ -8,6 +8,12 @@ instance_storage_address=http://localhost:9130/instance-storage/instances
 loan_type_storage_address=http://localhost:9130/loan-types
 material_type_storage_address=http://localhost:9130/material-types
 identifier_type_storage_address=http://localhost:9130/identifier-types
+creator_type_storage_address=http://localhost:9130/creator-types
+contributor_type_storage_address=http://localhost:9130/contributor-types
+instance_format_storage_address=http://localhost:9130/instance-formats
+instance_type_storage_address=http://localhost:9130/instance-types
+classification_type_storage_address=http://localhost:9130/classification-types
+
 
 for f in $datadir/mtypes/*.json; do
     curl -w '\n' -X POST -D - \
@@ -46,6 +52,52 @@ for f in $datadir/identifiertypes/*.json; do
          "${identifier_type_storage_address}"
 done
 
+for f in $datadir/creatortypes/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -H "X-Okapi-Token: ${token}" \
+         -d @$f \
+         "${creator_type_storage_address}"
+done
+
+for f in $datadir/contributortypes/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -H "X-Okapi-Token: ${token}" \
+         -d @$f \
+         "${contributor_type_storage_address}"
+done
+
+for f in $datadir/instanceformats/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -H "X-Okapi-Token: ${token}" \
+         -d @$f \
+         "${instance_format_storage_address}"
+done
+
+for f in $datadir/instancetypes/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -H "X-Okapi-Token: ${token}" \
+         -d @$f \
+         "${instance_type_storage_address}"
+done
+
+for f in $datadir/classificationtypes/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -H "X-Okapi-Token: ${token}" \
+         -d @$f \
+         "${classification_type_storage_address}"
+done
+
+
 for f in $datadir/instances/*.json; do
     curl -w '\n' -X POST -D - \
          -H "Content-type: application/json" \
@@ -54,5 +106,3 @@ for f in $datadir/instances/*.json; do
          -d @$f \
          "${instance_storage_address}"
 done
-
-
