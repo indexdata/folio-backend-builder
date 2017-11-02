b3b04c552f388ddc680b58dbc254ece4bfe4b767
Niels Erik G. Nielsen <nielserik@indexdata.com>
2017-11-02 10:14:25 +0100

    Upgrade to 6.0.0-SNAPSHOT of mod-inventory.

--- other-modules/inventory/unassign-undeploy-modules-localhost.sh
+++
@@ -1,4 +0,0 @@
-curl -X DELETE http://localhost:9130/_/proxy/tenants/diku/modules/mod-inventory-5.1.2-SNAPSHOT
-curl -X DELETE http://localhost:9130/_/proxy/tenants/diku/modules/mod-inventory-storage-5.1.1-SNAPSHOT
-curl -X DELETE http://localhost:9130/_/proxy/modules/mod-inventory-5.1.2-SNAPSHOT
-curl -X DELETE http://localhost:9130/_/proxy/modules/mod-inventory-storage-5.1.1-SNAPSHOT
