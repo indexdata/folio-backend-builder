17f2f8f3c69e2a7b0dd5f7cf7cd42c8ccd0b1d92
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-02-20 21:32:20 +0100

    Add master script

---
+++ create-tenant-db-deploy-auth-config-inventory-admin-import-token.sh
@@ -0,0 +1,28 @@
+./okapi/create-tenant.sh
+
+./drop-pgdb-okapi_modules-and-roles.sh
+
+./create-pgdb-okapi_modules.sh
+
+./authn-proxy-modules/register-all-module-proxies.sh
+
+./deployment-descriptors/deploy-assign-all-except-authtoken.sh
+
+./other-modules/proxy-deploy-assign-modules.sh
+
+./other-modules/inventory/proxy-deploy-assign-modules-localhost.sh
+
+./ui-modules/proxy-enable-ui-modules.sh
+
+./diku_admin/create-diku_admin.sh
+
+cd $FOLIO/mod-inventory-storage/sample-data/
+
+./import.sh diku
+
+$FOLIO/install-folio-backend/other-modules/graphql/proxy-deploy-assign-localhost.sh
+
+cd $FOLIO/install-folio-backend
+
+./deployment-descriptors/deploy-assign-authtoken.sh
+
