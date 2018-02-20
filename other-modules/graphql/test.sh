0fbd1352b98edb106ede8d611dde1c3278ad4b08
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-02-20 21:32:01 +0100

    Add graphql

---
+++ other-modules/graphql/test.sh
@@ -0,0 +1,7 @@
+token=$(/home/ne/folio/install-folio-backend/util-scripts/get-token-diku_admin.sh localhost:9130)
+
+curl -w '\n' -X POST -D - -H "Content-type: application/json" -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" -d '{"query": "query { users { id, username } }"}' http://localhost:9130/graphql
+
+curl -w '\n' -X POST -D - -H "Content-type: application/json" -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" -d '{"query": "query { groups { group, metadata { createdByUser { username } } } } "}' http://localhost:9130/graphql
+
+curl -w '\n' -X POST -D - -H "Content-type: application/json" -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" -d @instances-query.json  http://localhost:9130/graphql
