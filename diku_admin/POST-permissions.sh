186fae4a4a7b4fb04d7fc0bb891139ffb90f6704
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-03-16 20:06:41 +0100

    Log-message

--- diku_admin/POST-permissions.sh
+++ diku_admin/POST-permissions.sh
@@ -1,3 +1,3 @@
 workdir=$FOLIO/install-folio-backend/diku_admin
-
+echo POST permissions for user diku_admin
 curl -H "X-Okapi-Tenant: diku" -H "Content-type: application/json" -X POST -d@$workdir/permissions.json http://localhost:9130/perms/users
