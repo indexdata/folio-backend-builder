28cb1970c9280f465e0263f78311d70a422d75e6
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-09-09 11:48:09 +0200

    Manage mod-circulation

--- drop-pgdb-okapi_modules-and-roles.sh
+++ drop-pgdb-okapi_modules-and-roles.sh
@@ -4,4 +4,4 @@ sudo -u postgres bash -c "psql -c \"DROP ROLE diku_mod_users;\""
 sudo -u postgres bash -c "psql -c \"DROP ROLE diku_mod_login;\""
 sudo -u postgres bash -c "psql -c \"DROP ROLE diku_mod_configuration;\""
 sudo -u postgres bash -c "psql -c \"DROP ROLE diku_mod_inventory_storage;\""
-
+sudo -u postgres bash -c "psql -c \"DROP ROLE diku_mod_circulation_storage;\""
