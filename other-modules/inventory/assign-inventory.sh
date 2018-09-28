7ed129f97de18a330db8aad389132ed04231d624
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-09-28 14:26:25 +0200

    Upgrade mod-rtac version

--- other-modules/inventory/assign-inventory.sh
+++ other-modules/inventory/assign-inventory.sh
@@ -24,4 +24,4 @@ curl -w '\n' -X POST -D - -H "Content-type: application/json" -d '{"id": "mod-ci
 
 echo mod-rtac assign
 
-curl -w '\n' -X POST -D - -H "Content-type: application/json" -d '{"id": "mod-rtac-1.1.1-SNAPSHOT"}' http://localhost:9130/_/proxy/tenants/diku/modules
+curl -w '\n' -X POST -D - -H "Content-type: application/json" -d '{"id": "mod-rtac-1.2.0-SNAPSHOT"}' http://localhost:9130/_/proxy/tenants/diku/modules
