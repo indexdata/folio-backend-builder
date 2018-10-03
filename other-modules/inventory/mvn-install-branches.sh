b58edffd3f1aec5b425b7623375f09e1b6ec2bdd
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-10-03 14:57:38 +0200

    Add mvn install file without `git checkout master` for each module.

--- other-modules/inventory/mvn-install-branches.sh
+++ other-modules/inventory/mvn-install-branches.sh
@@ -1,30 +1,23 @@
 echo Install mod-inventory-storage
 cd $FOLIO/mod-inventory-storage
-git checkout master
 mvn clean install
 echo Install mod-inventory
 cd $FOLIO/mod-inventory
-git checkout master
 mvn clean install
 echo Install mod-codex-mux
 cd $FOLIO/mod-codex-mux
-git checkout master
 mvn clean install
 echo Install mod-codex-inventory
 cd $FOLIO/mod-codex-inventory
-git checkout master
 mvn clean install
 echo Install mod-circulation-storage
 cd $FOLIO/mod-circulation-storage
-git checkout master
 mvn clean install
 echo Install mod-circulation
 cd $FOLIO/mod-circulation
-git checkout master
 mvn clean install
 echo Install mod-rtac
 cd $FOLIO/mod-rtac
-git checkout master
 mvn clean install
 
 ls -l $FOLIO/mod-inventory-storage/target/mod-*SNAPSHOT.jar
