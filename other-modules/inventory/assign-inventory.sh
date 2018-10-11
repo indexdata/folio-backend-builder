0bf84fc0f0f14db5c8bcb6cd8e2ae682b14bdd41
Niels Erik G. Nielsen <nielserik@indexdata.com>
2018-10-11 22:19:02 +0200

    Update inventory-storage version

--- other-modules/inventory/assign-inventory.sh
+++ other-modules/inventory/assign-inventory.sh
@@ -1,6 +1,6 @@
 echo mod-inventory-storage assign
 
-curl -w '\n' -X POST -D - -H "Content-type: application/json" -d '{"id": "mod-inventory-storage-13.0.0-SNAPSHOT"}' http://localhost:9130/_/proxy/tenants/diku/modules
+curl -w '\n' -X POST -D - -H "Content-type: application/json" -d '{"id": "mod-inventory-storage-13.0.1-SNAPSHOT"}' http://localhost:9130/_/proxy/tenants/diku/modules
 
 echo mod-inventory assign
 
