e75945616d681829d29bcba2d1c6d0ac615a0ad4
Niels Erik G. Nielsen <nielserik@indexdata.com>
2020-02-28 20:03:06 +0100

    Fixes to get-token, get-request
      Parameterize protocol
      Remove ctrl-M after headers when getting token (issue with our SSL sites)
      Follow re-directs

--- util-scripts/get-request.sh
+++ util-scripts/get-request.sh
@@ -1,8 +1,20 @@
-host=$1
-request=$2
-echo request
-token=$($FOLIO/install-folio-backend/util-scripts/get-token-diku_admin.sh $host)
 
-echo $host/$request
+tenant=$1
+username=$2
+password=$3
+protocol=$4
+host=$5
+request=$6
+echo Tenant $tenant
+echo Username $username
+echo Protocol $protocol
+echo Host $host
+echo Request $request
+token=$($FOLIO/install-folio-backend/util-scripts/get-token.sh $tenant $username $password $protocol $host)
+echo Token: $token
+url=$protocol://$host$request
 
-curl -w -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $token" http://$host/$request
+echo URL: $url
+
+curl -L  "${url}"  -H "x-okapi-tenant: ${tenant}" -H 'content-type: application/json' -H "x-okapi-token: ${token}"
+
