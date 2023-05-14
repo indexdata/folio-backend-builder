23fb323557ebc1d691d81e10355df7d9f5aca57c
Niels Erik Nielsen <nielserik@indexdata.com>
2023-05-14 11:01:15 +0200

    Tweak port range given to Okapi for module deployment.

--- okapi/start-okapi.sh
+++ okapi/start-okapi.sh
@@ -1,3 +1,3 @@
 # Start Okapi
 
-/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java -Dport_start=9141 -Dport_end=9199 -Dstorage=postgres -jar $FOLIO/okapi/okapi-core/target/okapi-core-fat.jar dev
+/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java -Dport_start=9152 -Dport_end=9199 -Dstorage=postgres -jar $FOLIO/okapi/okapi-core/target/okapi-core-fat.jar dev
