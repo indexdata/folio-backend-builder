0b2d5e632afac5d6adce05192718f7e86f6abc91
Niels Erik Nielsen <nielserik@indexdata.com>
2023-05-16 14:11:37 +0200

    Validate: Verify directories and files exist

--- configutils/validate-config.sh
+++ configutils/validate-config.sh
@@ -7,7 +7,7 @@ else
   printf "\nValidating [$CONFIG_FILE]\n"
   parsed=$(jq -r '.' $CONFIG_FILE)
   if [[ -z "$parsed" ]]; then
-    printf "\n! Could not parse [$CONFIG_FILE]. Validatition cancelled.\n"
+    printf "\n! Could not validate [$CONFIG_FILE] due to parsing error.\n"
     exit
   fi
 fi
@@ -48,14 +48,16 @@ done
 echo "* Checking that Git checkout directories that are referenced by modules are also defined in the configuration and exist on the file system"
 requestedCheckoutDirs=$(jq -r '.moduleConfigs | unique_by(.checkedOutTo)[].checkedOutTo' "$CONFIG_FILE")
 for dir in $requestedCheckoutDirs; do
-  found=$(jq --arg dir "$dir" -r '.checkoutRoots | any(.symbol == $dir)' "$CONFIG_FILE")
-  if [[ "$found" != "true" ]]; then 
-     error="$error\nCheckout directory $dir is requested by a module but is not defined in 'checkoutRoots'"
-  else 
-     directory=$(jq --arg dir $dir --arg home $HOME -r '.checkoutRoots[] | select(.symbol == $dir).directory | sub("~";$home)' "$CONFIG_FILE") 
-     if [ ! -d $directory ]; then
-       error="$error\nSpecified check-out directory [$directory] not found on this file system"
-     fi
+  if [[ "$dir" != "null" ]]; then
+    found=$(jq --arg dir "$dir" -r '.checkoutRoots | any(.symbol == $dir)' "$CONFIG_FILE")
+    if [[ "$found" != "true" ]]; then 
+      error="$error\nCheckout directory $dir is requested by a module but is not defined in 'checkoutRoots'"
+    else 
+      directory=$(jq --arg dir $dir --arg home $HOME -r '.checkoutRoots[] | select(.symbol == $dir).directory | sub("~";$home)' "$CONFIG_FILE") 
+      if [ ! -d $directory ]; then
+        error="$error\nSpecified check-out directory [$directory] not found on this file system"
+      fi
+    fi
   fi
 done
 
@@ -68,22 +70,59 @@ for mod in $selectedModules; do
        "$CONFIG_FILE")
   if [[ -n "$found" ]]; then
     checkedOutToSymbol=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).checkedOutTo' "$CONFIG_FILE")
-    checkedOutTo=$(jq --arg symbol $checkedOutToSymbol --arg home $HOME -r '.checkoutRoots[] | select(.symbol == $symbol).directory | sub("~";$home)' "$CONFIG_FILE")
     methodSymbol=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).deployment.method' "$CONFIG_FILE")
-    pathToJar=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).deployment.pathToJar' "$CONFIG_FILE")
-    if [[ "$methodSymbol" == "DD" ]]; then
-      if [ ! -d "$checkedOutTo/$mod" ]; then
-         error="$error\nModule directory [$checkedOutTo/$mod] not found.\n"
-      elif [ ! -d "$checkedOutTo/$mod/target" ]; then
-         error="$error\n$mod's checkout directory found but it doesn't seem to be built (no /target) [$checkedOutTo/$mod/target]\n" 
-      elif [ ! -f "$checkedOutTo/$mod/$pathToJar" ]; then
-         error="$error\n$mod's checkout directory with subdir /target found but cannot find the requested jar file [$checkedOutTo/$mod/$pathToJar]\n" 
-      elif [ ! -f "$checkedOutTo/$mod/target/ModuleDescriptor.json" ]; then
-         error="$error\nFound module directory and jar file but cannot find a module descriptor at  $checkedOutTo/$mod/target/ModuleDescriptor.json\n"
+    if [[ "$checkedOutToSymbol" == "null" || "$methodSymbol" == "null"  ]]; then
+      if [[ "$checkedOutToSymbol" == "null" ]]; then
+        error="$error\nMissing configuration for $mod: 'checkedOutTo'.\n"
       fi
+      if [[ "$methodSymbol" == "null" ]]; then
+        error="$error\nMissing configuration for $mod: 'deployment.method'.\n"
+      fi
+    else 
+      checkedOutTo=$(jq --arg symbol $checkedOutToSymbol --arg home $HOME -r '.checkoutRoots[] | select(.symbol == $symbol).directory | sub("~";$home)' "$CONFIG_FILE")
+      if [[ ! -d "$checkedOutTo/$mod" ]]; then
+        error="$error\nNo check-out of $mod found at $checkedOutTo/$mod\n" 
+      fi
+      if [[ "$methodSymbol" == "DD" ]]; then
+        pathToJar=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).deployment.pathToJar' "$CONFIG_FILE")
+        jvm=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).deployment.jvm' "$CONFIG_FILE")
+        env=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).deployment.env' "$CONFIG_FILE")
+        if [ "$pathToJar" == "null" ]; then
+          error="$error\nMissing configuration for $mod: 'deployment.pathToJar'.\n"
+        fi      
+        if [ "$jvm" == "null" ]; then
+          error="$error\nMissing configuration for $mod: 'deployment.jvm'.\n"
+        fi 
+        if [ "$env" == "null" ]; then
+          error="$error\nMissing configuration for $mod: 'deployment.env'.\n"
+        fi 
+        if [[ -d "$checkedOutTo/$mod" && "$pathToJar" != "null" ]]; then
+          if [ ! -d "$checkedOutTo/$mod" ]; then
+            error="$error\nModule directory [$checkedOutTo/$mod] not found.\n"
+          elif [ ! -d "$checkedOutTo/$mod/target" ]; then
+            error="$error\n$mod's checkout directory found but it doesn't seem to be built (no /target) [$checkedOutTo/$mod/target]\n" 
+          elif [ ! -f "$checkedOutTo/$mod/$pathToJar" ]; then
+            error="$error\n$mod's checkout directory with subdir /target found but cannot find the requested jar file [$checkedOutTo/$mod/$pathToJar]\n" 
+          elif [ ! -f "$checkedOutTo/$mod/target/ModuleDescriptor.json" ]; then
+            error="$error\nFound module directory and jar file but cannot find a module descriptor at  $checkedOutTo/$mod/target/ModuleDescriptor.json\n"
+          fi
+        fi
+      fi
+      specifiedVersion=$(jq --arg name $mod -r '.selectedModules[] | select(.name == $name) | .version ' "$CONFIG_FILE")
+      if [[ -d "$checkedOutTo/$mod" &&  "$specifiedVersion" != "null" ]]; then
+        installedVersion=$(jq -r '.id' "$checkedOutTo/$mod/target/ModuleDescriptor.json")
+        if [ "$installedVersion" != "$mod-$specifiedVersion" ]; then
+          printf "\n**NOTE that version is not as specified : Said '$mod-$specifiedVersion' -> Got '$installedVersion'\n"
+        fi
+      fi  
+    fi
+    permissions=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).permissions' "$CONFIG_FILE")
+    if [[ "$permissions" == "null" ]]; then
+      error="$error\nMissing configuration for $mod: 'permissions'.\n"      
     fi
   fi
 done
+
 
 
 ### Report
@@ -94,14 +133,3 @@ else
 fi  
 
 
-# Other possible validations for each module: 
-#    - It has "name", 
-#             "version", 
-#             "checkedOutTo", 
-#             "requiredBy", 
-#             "deployment", 
-#             "deployment.method"
-#      - if "deployment.method" is not "DOCKER", that is has "jvm", "pathToJar"
-#        - if "deployment.method" is DD-PG or DD-PG-KAFKA, that it has "pgHost"
-#      - the jar file exists 
-#      - the descriptor JSONs exist
