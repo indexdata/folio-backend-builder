80bcf0c8be30e31835b71826886a627d4293d528
Niels Erik Nielsen <nielserik@indexdata.com>
2023-05-16 11:26:08 +0200

    Validate: Verify directories and files exist

--- configutils/validate-config.sh
+++ configutils/validate-config.sh
@@ -1,16 +1,21 @@
 CONFIG_FILE=$1
 
 if [[ -z "$CONFIG_FILE" ]]; then
-  echo "Please provide JSON config file to validate:  ./validate-config.sh myconf.json"
+  printf "\nPlease provide JSON config file to validate:  ./validate-config.sh myconf.json\n"
   exit
 else 
-  echo "Validating $CONFIG_FILE"
+  printf "\nValidating [$CONFIG_FILE]\n"
+  parsed=$(jq -r '.' $CONFIG_FILE)
+  if [[ -z "$parsed" ]]; then
+    printf "\n! Could not parse [$CONFIG_FILE]. Validatition cancelled.\n"
+    exit
+  fi
 fi
 
 error=""
 
 ###
-echo "Checking that configurations exist for all selected modules"
+echo "* Checking that configurations exist for all selected modules"
 selectedModules=$(jq -r '.selectedModules[] | select(.name != null) | .name ' "$CONFIG_FILE")
 for mod in $selectedModules; do
   found=$(jq --arg mod "$mod" \
@@ -23,7 +28,7 @@ for mod in $selectedModules; do
 done
 
 ###
-echo "Checking that the JVMs requested by modules are defined"
+echo "* Checking that the JVMs that are requested by modules are also defined in the configuration and exist on the file system"
 requestedJvms=$(jq -r '.moduleConfigs | unique_by(.deployment.jvm)[].deployment.jvm' "$CONFIG_FILE")
 for jvm in $requestedJvms; do
   if [[ ! "$jvm" == "null" ]]; then
@@ -40,7 +45,7 @@ for jvm in $requestedJvms; do
 done
 
 ###
-echo "Checking that all Git checkout directories referenced by modules are defined and exist."
+echo "* Checking that Git checkout directories that are referenced by modules are also defined in the configuration and exist on the file system"
 requestedCheckoutDirs=$(jq -r '.moduleConfigs | unique_by(.checkedOutTo)[].checkedOutTo' "$CONFIG_FILE")
 for dir in $requestedCheckoutDirs; do
   found=$(jq --arg dir "$dir" -r '.checkoutRoots | any(.symbol == $dir)' "$CONFIG_FILE")
@@ -54,9 +59,36 @@ for dir in $requestedCheckoutDirs; do
   fi
 done
 
+###
+echo "* Checking that selected modules are checked out and built"
+selectedModules=$(jq -r '.selectedModules[] | select(.name != null) | .name ' "$CONFIG_FILE")
+for mod in $selectedModules; do
+  found=$(jq --arg mod "$mod" \
+   -r '.moduleConfigs[] | select(.name == $mod)' \
+       "$CONFIG_FILE")
+  if [[ -n "$found" ]]; then
+    checkedOutToSymbol=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).checkedOutTo' "$CONFIG_FILE")
+    checkedOutTo=$(jq --arg symbol $checkedOutToSymbol --arg home $HOME -r '.checkoutRoots[] | select(.symbol == $symbol).directory | sub("~";$home)' "$CONFIG_FILE")
+    methodSymbol=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).deployment.method' "$CONFIG_FILE")
+    pathToJar=$(jq --arg mod "$mod" -r '.moduleConfigs[] | select(.name == $mod).deployment.pathToJar' "$CONFIG_FILE")
+    if [[ "$methodSymbol" == "DD" ]]; then
+      if [ ! -d "$checkedOutTo/$mod" ]; then
+         error="$error\nModule directory [$checkedOutTo/$mod] not found.\n"
+      elif [ ! -d "$checkedOutTo/$mod/target" ]; then
+         error="$error\n$mod's checkout directory found but it doesn't seem to be built (no /target) [$checkedOutTo/$mod/target]\n" 
+      elif [ ! -f "$checkedOutTo/$mod/$pathToJar" ]; then
+         error="$error\n$mod's checkout directory with subdir /target found but cannot find the requested jar file [$checkedOutTo/$mod/$pathToJar]\n" 
+      elif [ ! -f "$checkedOutTo/$mod/target/ModuleDescriptor.json" ]; then
+         error="$error\nFound module directory and jar file but cannot find a module descriptor at  $checkedOutTo/$mod/target/ModuleDescriptor.json\n"
+      fi
+    fi
+  fi
+done
+
+
 ### Report
 if [[ -z "$error" ]]; then
-  echo "Configuration OK"
+  printf "\nThe FOLIO config in [$CONFIG_FILE] looks good!\n"
 else 
   printf "\n\nERROR: $error\n"
 fi  
