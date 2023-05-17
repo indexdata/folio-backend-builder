cb952139819e09daeb87d9419013e6e384502ad0
Niels Erik Nielsen <nielserik@indexdata.com>
2023-05-17 18:21:17 +0200

    README. File naming.

--- okapi/init-db.sh
+++ okapi/init-db.sh
@@ -1,2 +1,2 @@
-/usr/lib/jvm/java-1.11.0-openjdk-amd64/bin/java -Dport=8600 -Dstorage=postgres -jar $FOLIO/okapi/okapi-core/target/okapi-core-fat.jar initdatabase
+/usr/lib/jvm/java-11-openjdk-amd64/bin/java -Dport=8600 -Dstorage=postgres -jar $FOLIO/okapi/okapi-core/target/okapi-core-fat.jar initdatabase
 
