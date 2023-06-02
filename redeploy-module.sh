8990a08b82e5f70c85c73cc2ebce3cd0053535a6
Niels Erik Nielsen <nielserik@indexdata.com>
2023-06-02 15:57:22 +0200

    Flag old jar file when re-deploying

--- redeploy-module.sh
+++ redeploy-module.sh
@@ -1,10 +1,28 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 workdir=$SCRIPT_DIR
-MOD=$1
-PROJECT=$2
+moduleName=$1
+projectFile=$2
 source "$workdir/lib/ConfigReader.sh"
 
-dd=$(makeDeploymentDescriptor "$MOD" "$PROJECT")
-id=$(moduleDescriptorId "$MOD" "$PROJECT")
+function filesAge() {
+  days="$((($(date +%s) - $(date +%s -r "$jarFile")) / 86400))"
+  hours="$((($(date +%s) - $(date +%s -r "$jarFile")) / 3600))"
+  minutes="$((($(date +%s) - $(date +%s -r "$jarFile")) / 60))"
+  if [[ $days -eq 0 ]]; then
+    if [[ $hours -gt 1 ]]; then
+      printf "\n\n  Last compiled %s hours ago." $hours
+    else
+      printf "   (Compiled %s minutes ago)" $minutes
+    fi
+  else
+    printf "\n\n    ** This jar was last compiled %s days ago ** " $days
+  fi
+}
+
+dd=$(makeDeploymentDescriptor "$moduleName" "$projectFile")
+id=$(moduleDescriptorId "$moduleName" "$projectFile")
 curl -X DELETE "http://localhost:9130/_/discovery/modules/$id"
 curl -w '\n' -D - -s -H "Content-type: application/json" -d "$dd" http://localhost:9130/_/discovery/modules
+
+jarFile=$(moduleDirectory "$moduleName" "$projectFile")/$moduleName/$(pathToJar "$moduleName" "$projectFile")
+printf "\n\nRedeployment of %s. %s\n\n" "$jarFile"  "$(filesAge "$jarFile")"
