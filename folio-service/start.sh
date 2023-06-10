01ef90ef61e0b7c9ae101d71eb6b25c01236c9ed
Niels Erik Nielsen <nielserik@indexdata.com>
2023-06-10 22:07:57 +0200

    Introduce options to spin-up and service start scripts
      - for running validate, clone-and-compile before installing, changing default Okapi directory or default Okapi log output file, forcing shutdown of a running Okapi without prompting first

--- folio-service/start.sh
+++ folio-service/start.sh
@@ -1,20 +1,40 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-pathToOkapi=${1:-${FOLIO:-~/folio}}  # path from ~/folio, $FOLIO, or $1
+pathToOkapi=~/folio
+logToFile=0
+logFile=
 
-if [ -d "$pathToOkapi/okapi/okapi-core/target" ]; then
-  printf "Stop Postgres and Kafka Docker service.\n"
+while getopts ":p:o:" option; do
+   case $option in
+      p) # Path to Okapi
+         pathToOkapi=$OPTARG;;
+      o) # Send output to log file
+         logToFile=1
+         logFile=${OPTARG:-"okapi.log"};;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+echo "path: $pathToOkapi"
+
+if [ -d "$pathToOkapi"/okapi/okapi-core/target ]; then
+  printf "Stop Postgres and Kafka services (docker).\n"
   docker compose -f "$SCRIPT_DIR"/docker-compose.yml down
   printf "Starting Postgres and Kafka.\n"
   docker compose -f "$SCRIPT_DIR"/docker-compose.yml up -d
-  "$SCRIPT_DIR"/okapi/init-db-start-okapi.sh "$pathToOkapi" >> okapi.log 2>&1 &
-  printf "Okapi starting ..."
-  sleep 10
-  if [[ "$(tail -1 okapi.log)" == *supertenant\ ok ]]; then
-    pid=$(pgrep -f "okapi-core/target/okapi-core-fat.jar")
-    printf "Okapi started in back-ground.\nLogs redirected to '%s'. Okapi PID is [%s] \n\n" "$SCRIPT_DIR/okapi.log" "$pid"
+  if [[ $logToFile -eq 1 ]]; then
+      "$SCRIPT_DIR"/okapi/init-db-start-okapi.sh "$pathToOkapi" >> "$logFile" 2>&1 &
+      printf "Okapi starting ..."
+      sleep 10
+      if [[ "$(tail -1 "$logFile")" == *supertenant\ ok ]]; then
+        pid=$(pgrep -f "okapi-core/target/okapi-core-fat.jar")
+        printf "Okapi started in back-ground.\nLogs redirected to '%s'. Okapi PID is [%s] \n\n" "$logFile" "$pid"
+      else
+        printf "Okapi started in back-ground.\nLogs redirected to '%s'.\n\n" "$logFile"
+      fi
   else
-    printf "Okapi started in back-ground.\nLogs redirected to '%s'.\n\n" "$SCRIPT_DIR/okapi.log"
+     "$SCRIPT_DIR"/okapi/init-db-start-okapi.sh "$pathToOkapi"
   fi
 else
-  printf "Could not find Okapi installation at %s/okapi/okapi-core/target\n" "$pathToOkapi"
+  printf "Could not find Okapi installation at %s/okapi/okapi-core/target.\n" "$pathToOkapi"
 fi
--- spin-up.sh
+++ spin-up.sh
@@ -1,24 +1,59 @@
-project=$1
-pathToOkapi=$2
-# -f force, kill, restart okapi if running
-# -v validate config
+project=${*: -1}
+pathToOkapi=~/folio
+
+# -f force, stop okapi if already running
+# -v validate config before installing
 # -c clone and/or compile missing modules
-# -o send okapi log to file
+# -l file to send okapi log to (default "okapi.log)
+# -p path to okapi check-out (default "~/folio")
+
+stopOkapi=0
+validateConfig=0
+cloneAndCompile=0
+
+while getopts ":fvcl:p:" option; do
+   case $option in
+      f) # Stop currently running Okapi 
+         stopOkapi=1;;
+      v) # Validate config first
+         validateConfig=1;;
+      c) # Clone and compile missing modules if any
+         cloneAndCompile=1;;
+      l) # file to send Okapi logs to
+         logFile=$OPTARG;;
+      p) # Path to Okapi
+          pathToOkapi=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
 okapiPid=$(pgrep -f okapi-core-fat.jar)
 
+if [[ $validateConfig -eq 1 ]]; then
+  ./validate-config.sh "$project"
+  printf "Press Enter to continue "; read -r;
+fi
+if [[ $cloneAndCompile  -eq 1 ]]; then
+  ./clone-and-compile-modules.sh "$project"
+fi
 if [[ -n "$okapiPid" ]]; then
-  printf "Okapi is already running.  (%s)" "$(pgrep -fa okapi-core-fat.jar)"
-  printf "\nShut down the running Okapi and install a new?"
-  read -r -p ' [y/N]? ' choice
-  choice=${choice:-N}
-  case "$choice" in
-    y|Y) ;;
-    *) printf "\nOkay. spin-up cancelled.\n"; exit 1;;
-  esac
+  if [[ $stopOkapi -eq 0 ]]; then
+    printf "Okapi is already running.  (%s)" "$(pgrep -fa okapi-core-fat.jar)"
+    printf "\nShut down the running Okapi and install a new?"
+    read -r -p ' [y/N]? ' choice
+    choice=${choice:-N}
+    case "$choice" in
+      y|Y) ;;
+      *) printf "\nOkay. spin-up cancelled.\n"; exit 1;;
+    esac
+  fi
   printf "Shutting down Okapi.\n"
   pkill -f okapi-core-fat.jar
 fi
-
-./folio-service/start.sh "$pathToOkapi"
-./install-modulese.sh "$project"
+printf "Start FOLIO service, Okapi at '%s', sending log to %s\n" "$pathToOkapi" "$logFile"
+./folio-service/start.sh -p "$pathToOkapi" -o "$logFile"
+printf "Installing modules\n"
+./install-modules.sh "$project"
 
